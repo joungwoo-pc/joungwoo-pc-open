08b888246776b4db873fe36c63a3fd7d52a24db5
joungwoo-pc <joungwoo.pc@gmail.com>
2025-10-01 11:38:43 +0900

    run_sandboxdocker.bat 만들기

---
+++ run_sandboxdocker.bat
@@ -0,0 +1,13 @@
+# 1) tar와 compose 파일 받기
+curl.exe -f -L -o "sandboxdocker.tar" "https://github.com/joungwoo-pc/joungwoo-pc-open/releases/download/build-latest/sandboxdocker.tar"
+
+curl.exe -f -L -o "docker-compose.yml" "https://github.com/joungwoo-pc/joungwoo-pc-open/releases/download/build-latest/docker-compose.yml"
+
+# 2) 이미지 로드
+wsl docker load -i sandboxdocker.tar
+
+# 3) 컨테이너 실행
+wsl docker compose up -d
+
+# 4) 컨테이너 접속 (시작 위치: /root/ext_volume)
+wsl docker compose exec sandboxdocker bash
